4d31f7a48ef6d87c978cf7d9e5bd7f99611c93e6
Mohit <Mohit@DESKTOP-HRR2RKV>
2023-03-09 11:35:58 +0530

    commit09.03.2023

--- Controllers/PortagebillController.cs
+++ Controllers/PortagebillController.cs
@@ -27,12 +27,30 @@ namespace crewlinkship.Controllers
             _context = context;
             _appEnvironment = appEnvironment;
         }
-        public IActionResult Index()
+        public IActionResult Index(int? vesselId, int? month)
         {
-           int vesselId = 138; int month = 2; int year = 2023; string ispromoted = "no"; string checkpbtilldate = "";
+            /*int vesselId = 138; int month = 2;*/
+            int year = 2023; string ispromoted = "no"; string checkpbtilldate = "";
+            var data = _context.PortageBillVMs.FromSqlRaw<PortageBillVM>("getPortageBill @p0, @p1, @p2, @p3, @p4", vesselId, month, year, ispromoted, checkpbtilldate);
+
+            ViewBag.vessel = new SelectList(_context.TblVessels, "VesselId", "VesselName");
+
+            ViewBag.vesselDetails = _context.TblVessels.Include(x => x.Flag).Include(x => x.Ship).Where(x => x.IsDeleted == false && x.VesselId == 75).FirstOrDefault();
+
+            ViewBag.vessels = _context.TblVessels.Where(x => x.IsDeleted == false && x.IsActive == false && x.VesselId == 75).ToList();
+
+            return View(data);
+        }
+
+        [HttpGet]
+        public IActionResult Index(int? vesselId, int? month, int? year)
+        {
+            string ispromoted = "no"; string checkpbtilldate = "";
             var data = _context.PortageBillVMs.FromSqlRaw<PortageBillVM>("getPortageBill @p0, @p1, @p2, @p3, @p4", vesselId, month, year, ispromoted, checkpbtilldate);
             ViewBag.vessel = new SelectList(_context.TblVessels, "VesselId", "VesselName");
+
             ViewBag.vesselDetails = _context.TblVessels.Include(x => x.Flag).Include(x => x.Ship).Where(x => x.IsDeleted == false && x.VesselId == 75).FirstOrDefault();
+
             ViewBag.vessels = _context.TblVessels.Where(x => x.IsDeleted == false && x.IsActive == false && x.VesselId == 75).ToList();
 
             return View(data);
