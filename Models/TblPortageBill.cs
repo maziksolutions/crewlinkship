dab13f0d81d49138819f428c41882671d2c71fcf
Vijay <Vijay@DESKTOP-Q8L8S7A>
2023-03-14 15:26:48 +0530

    Poratge bill changes

--- Models/TblPortageBill.cs
+++ Models/TblPortageBill.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 #nullable disable
 
@@ -56,5 +58,23 @@ namespace crewlinkship.Models
         public bool? IsHoldWageAllotment { get; set; }
 
         public virtual TblContract Contract { get; set; }
+    }
+
+    
+    public partial class tblPBBankAllotment
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int BankAllotmentId { get; set; }
+        public int Crew { get; set; }
+        public int VesselId { get; set; }
+        public int BankId { get; set; }
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+        public double? Allotments { get; set; }
+        public bool IsMidMonthAllotment { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime Recdate { get; set; }
+        public bool? IsPromoted { get; set; }
     }
 }
--- ViewModel/PortageBillVM.cs
+++ ViewModel/PortageBillVM.cs
@@ -167,5 +167,77 @@ namespace crewlinkship.ViewModel
         public bool IsAddPrevBal { get; set; }
         public decimal? MidMonthAllotment { get; set; }
     }
-
+    public class PortageBillBowVM
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int pid { get; set; }
+        public int? PortageBillId { get; set; }
+        public int? CrewId { get; set; }
+        public int? ContractId { get; set; }
+        public int? numberofday { get; set; }
+        public string CrewName { get; set; }
+        public string RankName { get; set; }
+        public string cdcno { get; set; }
+        public string Nationality { get; set; }
+        public int? Rankid { get; set; }
+        public int? AppliedCBA { get; set; }
+        public DateTime? Paydate { get; set; }
+        public DateTime? SignOnDate { get; set; }
+        public DateTime? From { get; set; }
+        public DateTime? To { get; set; }
+        public int? TotalDays { get; set; }
+        public string Currency { get; set; }
+        public decimal? BasicWages { get; set; }
+        public decimal? FixedGtOT { get; set; }
+        public decimal? SecurityAllow { get; set; }
+        public decimal? LeavePay { get; set; }
+        public decimal? UniformAllow { get; set; }
+        public decimal? PensionFund { get; set; }
+        public decimal? OtherAllow { get; set; }
+        public decimal? TotalWages { get; set; }
+        public int? CrewListId { get; set; }
+        public decimal? OTHours { get; set; }
+        public decimal? OtherEarnings { get; set; }
+        public int? TransitDays { get; set; }
+        public decimal? TransitWages { get; set; }
+        public decimal? CashAdvance { get; set; }
+        public decimal? BondedStores { get; set; }
+        public decimal? OtherDeductions { get; set; }
+        public int? Allotments { get; set; }
+        public int? PFAmount { get; set; }
+        public int? UDAmount { get; set; }
+        public int? WFAmount { get; set; }
+        public int? Deduction { get; set; }
+        public decimal? Reimbursement { get; set; }
+        public decimal? otrate { get; set; }
+        public decimal? extraot { get; set; }
+        public decimal? LeaveWagesBF { get; set; }
+        public decimal? TotalDeductions { get; set; }
+        public decimal? TotalPayable { get; set; }
+        public decimal? PrevMonthBal { get; set; }
+        public decimal? FinalBalance { get; set; }
+        public DateTime? SignOffDate { get; set; }
+        public string Remarks { get; set; }
+        public string Vessel { get; set; }
+        public int? Vesselid { get; set; }
+        public int? bankid { get; set; }
+        public int? Tax { get; set; }
+        public int? UDdeduction { get; set; }
+        public int? WFdeduction { get; set; }
+        public int? WHTdeduction { get; set; }
+        public decimal? authorityDeduction { get; set; }
+        public int? AllTotalDays { get; set; }
+        public DateTime? createddate { get; set; }
+        public DateTime? modifieddate { get; set; }
+        public string createdby { get; set; }
+        public string modifyby { get; set; }
+        public int IsLeaveWagesCF { get; set; }
+        public string Attachment { get; set; }
+        public decimal? Gratuity { get; set; }
+        public decimal? IndPFAmount { get; set; }
+        public decimal? AVC { get; set; }
+        public bool IsAddPrevBal { get; set; }
+        public decimal? MidMonthAllotment { get; set; }
+    }
 }
